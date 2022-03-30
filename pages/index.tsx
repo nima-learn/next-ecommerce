2cfd8d8e3fb1b0dfe3397ba8324617f48fb92572
nima <nimafanpage1380work@gmail.com>
2022-03-30 14:08:35 +0430

    end of AI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type {NextPage} from 'next'
 import play from "../test";
 import {useEffect} from "react";
+import typeCasting from "../typeCasting";
 
 const Home: NextPage = () => {
     useEffect(() => {
         play();
+        typeCasting();
     }, []);
 
     return (
--- test.ts
+++ test.ts
@@ -1,10 +1,34 @@
+import {type} from "os";
+
 type Person = {
     name : string,
     age : number
+}
+interface Car{
+    name : string,
+    speed : number
+}
+
+interface RaceCar extends Car  {
+    color : string,
+    speedLimit : number
+}
+
+interface luxuryCar {
+    color : string,
+    price : number
 }
 export default function play(){
     function log(person: Person) {
         console.log(person.name,person.age)
     }
-    log({name: "nima",age:20})
+    // log({name: "nima",age:20});
+    const logCar = (car: Car) => car;
+    const race_car : RaceCar = {
+        color : "#ffffff",
+        speedLimit : 20,
+        name : "ferrari",
+        speed : 10
+    }
+    console.log(logCar(race_car).speed);
 }
