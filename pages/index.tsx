a81b3fd476a29dddb88e1442e1fb4baff39eb4d3
nima <nimafanpage1380work@gmail.com>
2022-03-30 15:25:02 +0430

    first generic commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type {NextPage} from 'next'
 import {useEffect} from "react";
-import play from "../interstingTypes";
+import play from "../Generic";
 
 const Home: NextPage = () => {
     useEffect(() => {
