cccac3feee9d7f37b63a9f33509265cf2a32cca9
nima <nimafanpage1380work@gmail.com>
2022-03-30 17:03:44 +0430

    final generic type

--- Generic.ts
+++ Generic.ts
@@ -1,10 +1,63 @@
 import {string} from "prop-types";
-
-export default function play() {
-    // const printInfo = (someObject: {[key: string]: string | boolean}) => {
-    //     console.log(someObject);
-    // }
-    const printInfo = (someObject: any) => someObject;
-    console.log(printInfo(true));
-    // printInfo({age: "20",married: true})
+interface Person{
+    name: string;
 }
+class Student implements Person{
+    name = ""
+}
+interface car {
+    speed : number;
+}
+class Logger <T extends Person>{
+    log(items: Array<T> , callback : (item: T) => T){
+        return items.map(item => callback(item));
+    }
+}
+
+export default function play(){
+    const log = new Logger<Student>();
+    console.log(log.log([{name : ""}],(e) => e))
+}
+
+
+
+// interface Person{
+//     name: string;
+// }
+// class Student implements Person{
+//     name = ""
+// }
+// interface car {
+//     speed : number;
+// }
+// class Logger <T>{
+//     log(items: Array<T> , callback : (item: T) => T){
+//         return items.map(item => callback(item));
+//     }
+// }
+//
+// export default function play(){
+//     const log = new Logger<string>();
+//     console.log(log.log(["s","nima"],(e) => e+9999999))
+// }
+
+
+// class Logger {
+//     log(items: Array<string> , callback : any){
+//         // debugger
+//         items.map(item => callback(item));
+//     }
+// }
+//
+//
+// export default function play() {
+//     const logger = new Logger();
+//     // debugger
+//     logger.log(["s","a"],(item : any) => {
+//         // debugger
+//         console.log(item+555555);
+//     })
+//     // const iterate = (items : Array<string>) => items;
+//     // console.log(iterate(["sss"]));
+//
+// }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // ignoreBuildErrors: true,
+  typescript: {
+    ignoreBuildErrors: false
+  }
 }
 
 module.exports = nextConfig
